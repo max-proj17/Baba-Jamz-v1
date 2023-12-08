b776abf8f293952875552cb786b535b9e6113685
Maximelio-Finch <87384802+max-proj17@users.noreply.github.com>
2023-12-07 23:57:04 -0600

    trying to loop audio

--- baba-riffusion-app/src/App.js
+++ baba-riffusion-app/src/App.js
@@ -1,11 +1,10 @@
 import React, { useState } from 'react';
 import './App.css';
-// Import Axios if you're going to make the API call directly from here
 import axios from 'axios';
 
 function App() {
   const [prompt, setPrompt] = useState('');
-  const [music, setMusic] = useState(null);
+  const [audioUrl, setAudioUrl] = useState(null);
 
   const handlePromptChange = (event) => {
     setPrompt(event.target.value);
@@ -14,11 +13,8 @@ function App() {
   const handleGenerateMusic = async () => {
     try {
       const response = await axios.post('http://localhost:3001/generate-music', { prompt });
-      // Update state with the received audio and spectrogram URIs
-      setMusic({
-        audioUrl: response.data.audio,
-        spectrogramUrl: response.data.spectrogram
-      });
+      // Update state with the received audio URI
+      setAudioUrl(response.data.audio);
     } catch (error) {
       console.error('There was an error generating the music:', error);
     }
@@ -26,31 +22,30 @@ function App() {
 
   return (
     <div className="App">
-    <header className="App-header">
-      <h1>Generate Music with Riffusion</h1>
-      <input
-        className="input-text"
-        type="text"
-        value={prompt}
-        onChange={handlePromptChange}
-        placeholder="Enter a prompt for music generation"
-      />
-      <button
-        className="button"
-        onClick={handleGenerateMusic}
-      >
-        Generate Music
-      </button>
-      {music && (
-        <div>
-          <audio controls src={music.audioUrl}>
-            Your browser does not support the audio element.
-          </audio>
-          <img src={music.spectrogramUrl} alt="Spectrogram" />
-        </div>
-      )}
-    </header>
-  </div>
+      <header className="App-header">
+        <h1>Generate Music with Riffusion</h1>
+        <input
+          className="input-text"
+          type="text"
+          value={prompt}
+          onChange={handlePromptChange}
+          placeholder="Enter a prompt for music generation"
+        />
+        <button
+          className="button"
+          onClick={handleGenerateMusic}
+        >
+          Generate Music
+        </button>
+        {audioUrl && (
+          <div>
+            <audio controls loop src={audioUrl}>
+              Your browser does not support the audio element.
+            </audio>
+          </div>
+        )}
+      </header>
+    </div>
   );
 }
 
