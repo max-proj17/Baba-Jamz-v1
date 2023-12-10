fc11b654228fccbcd2f7875cb3ebfa48515acf83
Maximelio-Finch <87384802+max-proj17@users.noreply.github.com>
2023-12-09 21:24:48 -0600

    Added looping audio with fade in-out with ffmpeg!

--- Server/server.js
+++ Server/server.js
@@ -1,47 +1,98 @@
+
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
-// const axios = require('axios');
 const cors = require('cors');
-const Replicate = require("replicate"); // Import the Replicate client
-
+const Replicate = require('replicate');
+const ffmpeg = require('fluent-ffmpeg');
+const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
+const onFinished = require('on-finished');
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+// Serve files from the temp directory statically
+app.use('/temp', express.static(path.join(__dirname, 'temp')));
+
+// This directory will need to exist and will be used to store the audio files temporarily
+const TEMP_DIR = path.join(__dirname, '/temp');
+
+// Check if the TEMP_DIR exists, if not create it
+if (!fs.existsSync(TEMP_DIR)) {
+  fs.mkdirSync(TEMP_DIR, { recursive: true });
+  console.log("Directory Created!");
+}else{
+  console.log("Directory Exists!");
+}
+
 // Initialize the Replicate client with your API token
 const replicate = new Replicate({
-    auth: process.env.REPLICATE_API_TOKEN,
-  });
+  auth: process.env.REPLICATE_API_TOKEN,
+});
 
-console.log(process.env.REPLICATE_API_TOKEN);
+// Function to apply fade-in and fade-out effects
+function addFadeEffects(inputPath, outputPath) {
+  return new Promise((resolve, reject) => {
+    ffmpeg(inputPath)
+      .audioFilters(
+        'afade=t=in:st=0:d=1', // Fade-in starting at 0 seconds and lasting 1 second
+        'afade=t=out:st=4:d=1.5' // Fade-out starting at 4 seconds and lasting 1 second
+      )
+      .on('end', () => {
+        console.log('Processing finished!');
+        resolve(outputPath);
+      })
+      .on('error', (err) => {
+        console.error('Error processing audio:', err);
+        reject(err);
+      })
+      .save(outputPath);
+  });
+}
+
 // Endpoint to handle music generation
 app.post('/generate-music', async (req, res) => {
-    const { prompt } = req.body;
+  const { prompt } = req.body;
 
-    try {
-      // Run the model using the Replicate client
-      const output = await replicate.run(
-        "riffusion/riffusion:8cf61ea6c56afd61d8f5b9ffd14d7c216c0a93844ce2d82ac1c9ecc9c7f24e05",
-        {
-          input: {
-            prompt_a: prompt // Use the prompt from the request body
-            // Add any other parameters you want to specify, such as denoising, num_inference_steps, etc.
-          }
+  try {
+    // Run the model using the Replicate client
+    const output = await replicate.run(
+      "riffusion/riffusion:8cf61ea6c56afd61d8f5b9ffd14d7c216c0a93844ce2d82ac1c9ecc9c7f24e05",
+      {
+        input: {
+          prompt_a: prompt // Use the prompt from the request body
         }
-      );
-  
-      // Send back the response to the client
-      res.json({
-        audio: output.audio, // Make sure to access the properties correctly based on the API's response
-        spectrogram: output.spectrogram
+      }
+    );
+
+    // Download the audio file
+    const audioResponse = await axios.get(output.audio, { responseType: 'arraybuffer' });
+    const inputFilePath = path.join(TEMP_DIR, `${Date.now()}_input.mp3`);
+    fs.writeFileSync(inputFilePath, Buffer.from(audioResponse.data));
+
+    // Apply fade effects and return the processed audio file
+    const processedFilename = `${Date.now()}_processed.mp3`;
+    const processedFilePath = path.join(TEMP_DIR, processedFilename);
+    addFadeEffects(inputFilePath, processedFilePath)
+      .then(() => {
+        // Send the URL to the processed file
+        res.json({ audio: `/temp/${processedFilename}` });
+      })
+      .catch((error) => {
+        console.error('Error processing audio:', error);
+        res.status(500).send('Error processing audio');
       });
-    } catch (error) {
-      console.error('Error calling Riffusion API:', error.message);
-      res.status(500).send('Error generating music');
-    }
+
+  } catch (error) {
+    console.error('Error calling Riffusion API:', error.message);
+    res.status(500).send('Error generating music');
+  }
 });
+
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
--- baba-riffusion-app/src/App.js
+++ baba-riffusion-app/src/App.js
@@ -1,64 +1,20 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import axios from 'axios';
-import WaveSurfer from 'wavesurfer.js';
 
 function App() {
   const [prompt, setPrompt] = useState('');
-  const [audioUrl, setAudioUrl] = useState(null);
-  const [waveSurfer, setWaveSurfer] = useState(null);
-  const waveformRef = useRef(null);
+  const [audioUrl, setAudioUrl] = useState('');
+
   const handlePromptChange = (event) => {
     setPrompt(event.target.value);
   };
 
-  useEffect(() => {
-    if (audioUrl && !waveSurfer && waveformRef.current) {
-      const ws = WaveSurfer.create({
-        container: waveformRef.current,
-        loop: true,
-      });
-  
-      ws.load(audioUrl);
-  
-      ws.on('ready', () => {
-        ws.setVolume(0);
-        let currentVolume = 0;
-        //handle fade in by adjusting currentVolume value
-        const fadeInInterval = setInterval(() => {
-          currentVolume += 0.01; 
-          if (currentVolume >= 1) {
-            clearInterval(fadeInInterval);
-            ws.setVolume(1);
-          } else {
-            ws.setVolume(currentVolume);
-          }
-        }, 200); //interval at which volume changes. smaller value -> faster changes 
-      });
-  
-      ws.on('finish', () => {
-        let currentVolume = 1;
-        const fadeOutInterval = setInterval(() => {
-          currentVolume -= 0.002; 
-          if (currentVolume <= 0) {
-            clearInterval(fadeOutInterval);
-            ws.setVolume(0);
-            ws.play(); 
-          } else {
-            ws.setVolume(currentVolume);
-          }
-        }, 200); 
-      });
-  
-      setWaveSurfer(ws);
-    }
-  }, [audioUrl, waveSurfer, waveformRef]);
-
-
   const handleGenerateMusic = async () => {
     try {
       const response = await axios.post('http://localhost:3001/generate-music', { prompt });
-      setAudioUrl(response.data.audio);
+      const fullAudioUrl = `http://localhost:3001${response.data.audio}`;
+      setAudioUrl(fullAudioUrl);
     } catch (error) {
       console.error('There was an error generating the music:', error);
     }
@@ -66,7 +22,6 @@ function App() {
 
   return (
     <div className="App">
-      <div id="waveform" ref={waveformRef}></div>
       <header className="App-header">
         <h1>Generate Music with Riffusion</h1>
         <input
@@ -76,18 +31,13 @@ function App() {
           onChange={handlePromptChange}
           placeholder="Enter a prompt for music generation"
         />
-        <button
-          className="button"
-          onClick={handleGenerateMusic}
-        >
+        <button className="button" onClick={handleGenerateMusic}>
           Generate Music
         </button>
         {audioUrl && (
-          <div>
-            <audio controls loop src={audioUrl}>
-              Your browser does not support the audio element.
-            </audio>
-          </div>
+          <audio controls loop src={audioUrl}>
+            Your browser does not support the audio element.
+          </audio>
         )}
       </header>
     </div>
@@ -95,3 +45,5 @@ function App() {
 }
 
 export default App;
+
+
